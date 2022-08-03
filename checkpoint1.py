9986ce21124c375311b38cbb67f34e0730c322df
Tyler Vo <tyler.k.vo@gmail.com>
2022-08-03 19:34:34 -0400

    Update

--- checkpoint1.py
+++
@@ -1,8 +0,0 @@
-# I am unsure of the details as of now, but I think I want to make either
-# a ping-pong game which is similar to pong but adds gravity. However, i cant think
-# of optional features to complete this.
-# or a dungeon game where I use animation, have enemies, collectables such as
-# coins, and a health bar. I also might decide to have more than one level, and a save
-# point later on. I look forward to this project and am excited to try different things.
-# for the dungeon thing, i am thinking of a game where you need to find a key, and unlock
-# the door while evading the enemies.
